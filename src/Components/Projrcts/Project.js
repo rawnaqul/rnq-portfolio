ba91a3722853ca6253e36e697e3455fc6761a8f5
rawnaqul <slow.coach.rawnaq@gmail.com>
2022-12-14 14:09:02 +0600

    ss display

--- src/Components/Projrcts/Project.js
+++ src/Components/Projrcts/Project.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import booki from '../../assets/bookish-hero-img(Screenshot 2022-12-11 013142).jpg'
+
 
 const Project = ({ project }) => {
 
@@ -7,16 +9,16 @@ const Project = ({ project }) => {
 
     return (
         <div className='grid grid-cols-12 gap-0 mb-7'>
-            <div className='col-span-5 bg-red-400 p-8'>
+            <div className='col-span-5 bg-red-600 p-8'>
                 <h5>{category}</h5>
                 <h3>{name}</h3>
                 <p>{details}</p>
                 <a href={link}>Live Site</a>
             </div>
-            <div className='col-span-7 bg-green-400'>
-                <img src={image} alt='project SS'></img>
+            <div className='col-span-7 bg-green-500'>
+                <img src={booki} alt='project SS'></img>
             </div>
-        </div>
+        </div >
     );
 };
 
