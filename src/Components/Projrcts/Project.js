df380aa5aaafe797b1d678fd3c06b0d1bc0c2381
rawnaqul <slow.coach.rawnaq@gmail.com>
2022-12-14 20:13:47 +0600

    live links fixed

--- src/Components/Projrcts/Project.js
+++ src/Components/Projrcts/Project.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Project.css'
 
 
 const Project = ({ project, colorcode }) => {
 
-    const { id, name, image, details, description, color, link, category } = project;
-    console.log(colorcode, 'kkkk');
+    const { id, name, image, details, description, color, link, category, mod } = project;
+    console.log(mod, 'kkkk');
 
     return (
-        <div className={`grid grid-cols-12 gap-0 mb-7 bg-slate-500`}>
-            <div className={`col-span-5 p-8 relative ${color}`}>
-                <h5 className='mb-5 text-base font-semibold'>{category}</h5>
-                <h3 className='font-mod text-2xl my-5'>{name}</h3>
+        <div className={`grid grid-cols-12 gap-0 mb-7 `}>
+            <div className={`col-span-5 p-8 relative ${mod}`}>
+                <h5 className='mb-5 text-slate-200 font-semibold'>{category}</h5>
+                <h3 className='font-mod text-2xl my-5 text-white'>{name}</h3>
                 <p>{details}</p>
-                <a href={link} className='absolute bottom-0 mb-8 border p-1 hover:bg-slate-300 hover:text-black' target='_blank' rel='noreferrer'>Live Site</a>
+                <a href={link} className='absolute bottom-0 mb-8 border p-1 hover:opacity-50 hover:bg-slate-500 hover:text-black' target='_blank' rel='noreferrer'>Live Site</a>
             </div>
             <div className='col-span-7 '>
                 <img src={image} alt='project SS'></img>
